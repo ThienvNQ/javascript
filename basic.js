94453608e8450281a7db6d1bc295dec593013e9a
Nguyễn Quốc Thiện <thienvnq@MacBook-Pro-cua-Thien.local>
2023-12-05 22:30:44 +0900

    source javascript Hello World

---
+++ basic.js
@@ -0,0 +1,23 @@
+console.log('Hello World, My name is ThienvNQ')
+const name = "ThienvNQ"
+const ThienvNQ = {
+    firstName: "Thien", 
+    lastName: "vNQ",
+    age: 21,
+    male: true,
+}
+
+//let undefinedTest;
+//let nullTest = null;
+
+if(ThienvNQ.age == 21) {
+    console.log("2023/12 Developerになる！")
+} else
+ if(ThienvNQ.age == 22) {
+    console.log("Se co N2")
+} else {
+    console.log("khong biet")
+}
+//console.log({ThienvNQ, undefinedTest, nullTest})
+console.log(name)
+console.log(ThienvNQ)
